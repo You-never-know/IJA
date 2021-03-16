8d88239be1757a6523abebeec44c9e66b94f76b7
rinaen <katka.nep@gmail.com>
2021-03-16 22:11:00 +0100

    Forklift skeleton

---
+++ src/Forklift.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Forklift {
+
+    private int id;
+    private Request request;
+    List<Goods> goodsList;
+    private int[] coordinates;
+    private int [][] path;
+    private int weightBearing;
+    private int piecesBearing;
+
+
+    public Forklift(int id, int[] coordinates) {
+        this.id = id;
+        this.coordinates = coordinates;
+    }
+
+}
