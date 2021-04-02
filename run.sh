ee2ec7a41bfa76a0c7d4fa8ccfe5f040e7645a71
You-never-know <xmarek72@stud.fit.vutbr.cz>
2021-04-02 23:43:27 +0200

    run.sh now translates and runs the application, our first window works

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+SRC="src"
 LIB="./lib/javafx-sdk-16/lib"
 
-java -cp build:${LIB} company.StoreManager
+if [[ `find . -type d -name "build" | wc -l` -eq 0 ]] ; then
+	mkdir build
+fi
+
+javac -classpath ${SRC} --module-path ${LIB} --add-modules javafx.controls,javafx.fxml  -d build ${SRC}/company/StoreManager.java
+
+java -cp build --module-path ${LIB} --add-modules javafx.controls,javafx.fxml company.StoreManager
--- src/company/StoreManager.java
+++ src/company/StoreManager.java
@@ -5,15 +5,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class StoreManager extends Application {
 
 	@Override
-	public void start(Stage primaryStage) throws Exception{
-		Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-		primaryStage.setTitle("Hello World");
-		primaryStage.setScene(new Scene(root, 300, 275));
+	public void start(Stage primaryStage) {
+		GridPane root = new GridPane();
+		Scene scene = new Scene(root, 1600, 1200);
+		primaryStage.setTitle("My warehouse");
+		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 
