bb57f30de13566c527ac9404e1e31e8a06b3176e
rinaen <katka.nep@gmail.com>
2021-04-09 00:18:03 +0200

    Coordinates javadoc

--- src/company/store/shelve/goods/coordinates/Coordinates.java
+++ src/company/store/shelve/goods/coordinates/Coordinates.java
@@ -3,27 +3,46 @@ package company.store.shelve.goods.coordinates;
 import java.lang.Math;
 import java.util.Objects;
 
+/**
+ *  Class for storing coordinates according to warehouse map
+ */
 public class Coordinates {
     private int x;
     private int y;
 
+    /**
+     * @param x int number to be set as x-coordinate of the Coordinates class instance
+     * @param y int number to be set as y-coordinate of the Coordinates class instance
+     */
     public Coordinates(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * @return x-coordinate of the Coordinates class instance
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * @return y-coordinate of the Coordinates class instance
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * @param x int number to be set as x-coordinate of the Coordinates class instance
+     */
     public void setX(int x) {
         this.x = x;
     }
 
+    /**
+     * @param y int number to be set as y-coordinate of the Coordinates class instance
+     */
     public void setY(int y) {
         this.y = y;
     }
@@ -32,6 +51,10 @@ public class Coordinates {
         System.out.println(this.getX() + ", " + this.getY());
     }
 
+    /**
+     * @param o Coordinates object for comparison with this Coordinates class instance
+     * @return boolean value corresponding to equality
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -40,19 +63,34 @@ public class Coordinates {
         return x == that.x && y == that.y;
     }
 
+    /**
+     * @return hash value of the Coordinates class instance object
+     */
     @Override
     public int hashCode() {
         return Objects.hash(x, y);
     }
 
+    /**
+     * @param origin Coordinates of the start position
+     * @return move value of the A* algorithm for start and current position
+     */
     public int getMoveValue(Coordinates origin) {
         return Math.abs(this.x - origin.getX()) + Math.abs(this.y - origin.getY());
     }
 
+    /**
+     * @param destination Coordinates of the final position
+     * @return cost value of the A* algorithm for start and final position
+     */
     public double getCostValue(Coordinates destination) {
         return (Math.sqrt(Math.pow(Math.abs(this.x - destination.getX()), 2) + Math.pow(Math.abs(this.y - destination.getY()), 2)));
     }
 
+    /**
+     * @param coordinates Coordinates for computing the heuristic value
+     * @return heuristic value of the A* algorithm for start and final position
+     */
     public double getHeuristicValue(Coordinates coordinates) {
         return this.getMoveValue(coordinates) + this.getCostValue(coordinates);
     }
